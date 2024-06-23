ec3be038a1db5b4fff8236ef0fb8f59b770b4f20
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-06-23 14:06:51 -0300

    feat: week 2 - readabiliry

--- w2/readability/readability.c
+++ w2/readability/readability.c
@@ -1,7 +1,60 @@
 #include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+bool issentence(char chr)
+{
+  return (
+    chr == '.' ||
+    chr == '!' ||
+    chr == '?'
+  );
+}
+
+void counter_lsw(int *lsw, string text)
+{
+  int l = 0;
+  int s = 0;
+  int w = 1;
+
+  for (int i = 0; i < strlen(text); i++) {
+    char chr = text[i];
+
+    if (isalpha(chr)) l++;
+    if (issentence(chr)) s++;
+    if (isspace(chr)) w++;
+  }
+
+  lsw[0] = l;
+  lsw[1] = s;
+  lsw[2] = w;
+}
 
 int main(void)
 {
-  string s = get_string("Text: ");
-  return 0;
-}
+  string text = get_string("Text: ");
+
+  int lsw[3];
+
+  counter_lsw(lsw, text);
+
+  int letters = lsw[0];
+  int sentences = lsw[1];
+  int words = lsw[2];
+
+  float L = ((float)letters / words) * 100;
+  float S = ((float)sentences / words) * 100;
+
+  int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+  if (index < 1)
+    printf("Before Grade 1");
+  else if (index >= 1 && index <= 16)
+    printf("Grade %i", index);
+  else if (index > 16)
+    printf("Grade 16+");
+
+  printf("\n");
+  }

e8ff28abc15a1160bfe350444b0215b8f52454d0
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-06-24 19:00:40 -0300

    fix: week 1 exercicies

--- w1/cash/cash.c
+++ w1/cash/cash.c
@@ -1,36 +1,38 @@
-# include <stdio.h>
-# include <cs50.h>
+#include <cs50.h>
+#include <stdio.h>
 
 int get_cents()
 {
-  int cents = 0;
+    int cents = 0;
 
-  do
-  {
-    cents = get_int("Change owed: ");
-  } while (cents <= 0);
+    do
+    {
+        cents = get_int("Change owed: ");
+    }
+    while (cents <= 0);
 
-  return cents;
+    return cents;
 }
 
 int main(void)
 {
-  int cents = get_cents();
-  int coins = 0;
+    int cents = get_cents();
+    int coins = 0;
     int rest = 0;
 
-  int values[4] = { 25, 10, 5, 1 };
+    int values[4] = {25, 10, 5, 1};
 
-  for (int i = 0; i < 4; i++)
-  {
-    int value = values[i];
+    for (int i = 0; i < 4; i++)
+    {
+        int value = values[i];
 
-    if (cents >= value) {
-      rest = cents % value;
-      coins += (cents - rest) / value;
-      cents = rest;
+        if (cents >= value)
+        {
+            rest = cents % value;
+            coins += (cents - rest) / value;
+            cents = rest;
+        }
     }
-  }
 
-  printf("%i\n", coins);
+    printf("%i\n", coins);
 }
--- w1/mario-more/mario.c
+++ w1/mario-more/mario.c
@@ -1,36 +1,38 @@
-# include <stdio.h>
-# include <cs50.h>
+#include <cs50.h>
+#include <stdio.h>
 
 int get_height()
 {
-  int n;
+    int n;
 
-  do
-  {
-    n = get_int("Height: ");
-  }
-  while (n < 1);
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1);
 
-  return n;
+    return n;
 }
 
 void print_row(int n, string chr)
 {
-  for (int i = 0; i < n; i++) {
-    printf("%s", chr);
-  }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s", chr);
+    }
 }
 
 int main(void)
 {
-  int h = get_height();
+    int h = get_height();
 
-  for (int i = 1; i <= h; i++) {
-    print_row(h-i, " ");
-    print_row(i, "#");
-    printf(" ");
-    print_row(i, "#");
+    for (int i = 1; i <= h; i++)
+    {
+        print_row(h - i, " ");
+        print_row(i, "#");
+        printf("  ");
+        print_row(i, "#");
 
-    printf("\n");
-  }
+        printf("\n");
+    }
 }

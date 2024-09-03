ca8b89e5e60ade7f3b8417ac722b16ec0dd689c9
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-09-03 19:58:04 -0300

    feat: sentimental readability added in python

---
+++ w6/sentimental-readability/readability.py
@@ -0,0 +1,40 @@
+import re
+
+from cs50 import get_string
+
+
+def print_grade(text: str):
+    letters = len(re.findall(r"[A-Za-z]", text))
+    words = text.count(' ') + 1
+    sentences = sum(
+        [
+            text.count('.'),
+            text.count('!'),
+            text.count('?'),
+        ]
+    )
+
+    L = (letters / words) * 100
+    S = (sentences / words) * 100
+
+    grade = round(0.0588 * L - 0.296 * S - 15.8)
+
+    if grade < 1:
+        print("Before Grade 1");
+    elif 1 <= grade <= 16:
+        print(f"Grade {grade}");
+    elif grade > 16:
+        print("Grade 16+");
+
+
+def main():
+    text = ''
+
+    while len(text) == 0:
+        text = get_string("Text: ")
+
+    print_grade(text)
+
+
+if __name__ == "__main__":
+    main()
